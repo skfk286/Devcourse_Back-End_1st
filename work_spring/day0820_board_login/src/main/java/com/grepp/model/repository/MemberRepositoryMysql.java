77f7f5b235d6a55d6e06f9dcfba2f3c7a1f5b233
YeChan Jung <skfk286@hanmail.net>
2024-08-20 17:08:19 +0900

    커밋

--- work_spring/day0820_board_login/src/main/java/com/grepp/model/repository/MemberRepositoryMysql.java
+++ work_spring/day0820_board_login/src/main/java/com/grepp/model/repository/MemberRepositoryMysql.java
@@ -15,7 +15,7 @@ public class MemberRepositoryMysql implements MemberRepository {
     @Override
     public String selectOne(String userid, String userpw) throws SQLException {
         // 원래는 여기서 DB 작업이 필요.
-        if("programmers".equals(userid) && "1234".equals(userpw)) {
+        if(("admin".equals(userid) || "programmers".equals(userid)) && "1234".equals(userpw)) {
             return userid;
         }
         return null;
