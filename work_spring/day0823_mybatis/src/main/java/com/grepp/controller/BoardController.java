1234d59eae97dcaf5c5d96e5085ba5c1e4af8002
Yechan Jung <skfk286@hanmail.net>
2024-08-27 09:45:19 +0900

    xml 상에서 insert 쿼리 진행 시에 외래키 설정을 위한 useGeneratedKeys, keyColumn, keyProperty 설정 추가.

--- work_spring/day0823_mybatis/src/main/java/com/grepp/controller/BoardController.java
+++ work_spring/day0823_mybatis/src/main/java/com/grepp/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.grepp.model.dto.BoardDTO;
 import com.grepp.model.dto.FileDTO;
 import com.grepp.model.service.BoardService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,7 @@ public class BoardController {
     @RequestMapping("/write.do") // 글쓰기
     public ModelAndView write(
             BoardDTO board,
-            HttpServletRequest request,
+            HttpSession session,
             @RequestPart(value = "uploadFile", required = false) MultipartFile[] uploadFile) throws SQLException {
         System.out.println(uploadFile[0].getOriginalFilename());
         System.out.println(uploadFile[1].getOriginalFilename());
@@ -57,7 +58,7 @@ public class BoardController {
         System.out.println("/write.do 호출 >");
         ModelAndView mav = new ModelAndView("alert");
 
-        board.setWriter((String) request.getSession().getAttribute("loginId"));
+        board.setWriter((String) session.getAttribute("loginId"));
         int result = boardService.write(board); // 첨부파일 저장 이전에 일단 게시글 부터 작성완료 되어야 한다.
 
         System.out.println("글 작성 결과 : " + result);
@@ -83,6 +84,10 @@ public class BoardController {
         return mav;
     }
 
+    /*
+        TODO : 객체지향 설계 레이어 상,
+                파일처리 까지 컨트롤러에 구현되었는데 -> 유틸리티 클래스로 분리시킬 필요성이 있어보임.
+     */
     private List<FileDTO> saveFiles(MultipartFile[] uploadFile) throws IOException {
         List<FileDTO> fileDTOList = new ArrayList<>();
         if (uploadFile != null && uploadFile.length > 0) { // 첨부된 파일이 확실히 있는 경우 저장절차 진행
