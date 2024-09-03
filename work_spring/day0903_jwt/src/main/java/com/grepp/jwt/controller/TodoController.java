b57b6541586ecd044ce0fc3304982e90d5806435
Yechan Jung <skfk286@hanmail.net>
2024-09-03 12:24:52 +0900

    JWT 를 테스트하기 위한 JPA 선행작업.

---
+++ work_spring/day0903_jwt/src/main/java/com/grepp/jwt/controller/TodoController.java
@@ -0,0 +1,47 @@
+package com.grepp.jwt.controller;
+
+import com.grepp.jwt.model.dto.TodoDTO;
+import com.grepp.jwt.model.entity.TodoEntity;
+import com.grepp.jwt.model.entity.UserEntity;
+import com.grepp.jwt.model.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+    final String tempUser = "programmers";
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<?> todoList() {
+        List<TodoEntity> entities = todoService.todoList(tempUser); // 추후 로그인 정보를 바탕으로 조회되게 수정해야 함.
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todoDTO) {
+        TodoEntity todoEntity = new TodoEntity();
+        todoEntity.setUsername(tempUser); /* TODO : 로그인 구현하고 수정해야 한다. */
+
+        List<TodoEntity> entities = todoService.create(todoEntity); // 추가된 _todo 까지 포함해서 갱신된 _todo 목록
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTodo(@RequestBody TodoDTO todoDTO) {
+        TodoEntity todoEntity = todoDTO.toEntity();
+        todoEntity.setUsername(tempUser);
+        return ResponseEntity.ok().body(todoService.update(todoEntity));
+    }
+}
---
+++ work_spring/day0903_jwt/src/main/java/com/grepp/jwt/model/service/TodoService.java
@@ -0,0 +1,46 @@
+package com.grepp.jwt.model.service;
+
+import com.grepp.jwt.model.entity.TodoEntity;
+import com.grepp.jwt.model.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<TodoEntity> create(TodoEntity todoEntity) { // 할일 생성
+        validate(todoEntity);
+
+        todoRepository.save(todoEntity);
+        return todoRepository.findByUsername(todoEntity.getUsername()); // 방금 추가한거 포함해서 할일 목록
+    }
+
+    public List<TodoEntity> todoList(String username) {
+        return todoRepository.findByUsername(username);
+    }
+
+    public TodoEntity update(TodoEntity todoEntity) {
+        TodoEntity original = todoRepository.findById(todoEntity.getId());
+
+        if(todoEntity.getTitle() != null) // 할일 제목을 새로 입력했네요? : 갱신
+            original.setTitle(todoEntity.getTitle());
+        if(original.isDone() != todoEntity.isDone()) // 수행 여부가 달려졌네요? : 갱신
+            original.setDone(todoEntity.isDone());
+
+        todoRepository.save(original);
+
+        return todoRepository.findById(original.getId());
+    }
+
+    private void validate(TodoEntity todoEntity) { // entity 유효한지 체크하기
+        if(todoEntity == null)
+            throw new RuntimeException("엔티티 null 이다.");
+        if(todoEntity.getUsername() == null)
+            throw new RuntimeException("정체 불명의 사용자가 엔티티 생성 요청됨.");
+
+    }
+}
